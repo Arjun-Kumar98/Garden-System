e8abee2ada9a45df960cec4d490e8334942305f9
Your Name <you@example.com>
2024-06-03 22:57:42 -0500

    Initial commit

---
+++ src/test/java/com/example/demo/MySpringBootGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class MySpringBootGardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
