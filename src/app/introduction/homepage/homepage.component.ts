36ad92b8a35b3b48b09078c55f4cf03ee89d5200
Your Name <you@example.com>
2024-06-07 17:00:44 -0500

    integrated the sign-up api

--- src/app/introduction/homepage/homepage.component.ts
+++ src/app/introduction/homepage/homepage.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { IntroductionModule } from '../introduction.module';
+import { ApiService } from '../../core/api.service';
 
 @Component({
   selector: 'app-homepage',
@@ -8,8 +12,34 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 title:any;
+newuser:any;
+newpassword:any;
+confpassword:any;
+emailaddress:any;
+login:boolean=true;
+
+constructor(private apiService:ApiService){}
 ngOnInit(){
   console.log("The page has loaded");
+  this.login = true;
   this.title="Welcome to Garden Management System";
+}
+signup(){
+  this.login = false;
+}
+register(){
+  const data={
+    "username":this.newuser,
+    "userpassword":this.newpassword,
+    "useremail":this.emailaddress
+  }
+  this.apiService.signUp(data).subscribe({
+      next:(response)=>{
+        console.log("The user is registered successfully");
+      },
+      error:(error)=>{
+        console.error("Error registering user",error);
+      }
+});
 }
 }
--- src/app/introduction/introduction.module.ts
+++ src/app/introduction/introduction.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { IntroductionroutingModule } from './introduction_routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ApiService } from '../core/api.service';
+import { provideHttpClient } from '@angular/common/http';
+
 
 
 
@@ -9,8 +13,10 @@ import { HomepageComponent } from './homepage/homepage.component';
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
-    IntroductionroutingModule
-    
-  ]
+    FormsModule,
+    IntroductionroutingModule,
+
+  ],
+providers:[ApiService,provideHttpClient()],
 })
 export class IntroductionModule { }
