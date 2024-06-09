631f827f9512bf796869b917bc50ff6ac1951cfb
Your Name <you@example.com>
2024-06-08 22:09:59 -0500

    page routing

--- src/app/introduction/signup/signup.component.ts
+++ src/app/introduction/signup/signup.component.ts
@@ -2,32 +2,23 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import { IntroductionModule } from '../introduction.module';
-import {Router} from '@angular/router'
 import { ApiService } from '../../core/api.service';
 
 @Component({
-  selector: 'app-homepage',
-  standalone: false,
-  templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
 })
-export class HomepageComponent {
-title:any;
-newuser:any;
-newpassword:any;
-confpassword:any;
-emailaddress:any;
-login:boolean=true;
+export class SignupComponent {
+  title:any;
+  newuser:any;
+  newpassword:any;
+  confpassword:any;
+  emailaddress:any;
+  
+constructor(private apiService:ApiService){}
 
-constructor(private apiService:ApiService, private router:Router){}
-ngOnInit(){
-  console.log("The page has loaded");
-  this.login = true;
-  this.title="Welcome to Garden Management System";
-}
-signup(){
-  this.router.navigate(['/Signup']);
-}
+
 register(){
   const data={
     "username":this.newuser,
