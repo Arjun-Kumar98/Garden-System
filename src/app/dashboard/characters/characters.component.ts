f1baa1b5bc544bedbf2804018b3a378e2617208f
Arjun Kumar <mrarjunkumar42@gmail.com>
2024-07-02 13:43:27 -0700

    update inventory details api

--- src/app/dashboard/characters/characters.component.ts
+++ src/app/dashboard/characters/characters.component.ts
@@ -15,12 +15,15 @@ userId:any;
 newrows:{itemId:number,itemname:string,itemqty:number}[]=[];
 rows:{itemId:number,itemname:string,itemqty:number}[]=[];
 updaterows:{itemId:number,itemname:string,itemqty:number}[]=[];
+deleterows:{itemId:number,itemname:string,itemqty:number}[]=[];
 constructor(private apiService:ApiService){}
 ngOnInit(){
   this.addrow();
   this.message="";
   this.userId = window.localStorage.getItem("userId");
   this.view_details();
+  this.rows=[];
+  this.updaterows=[];
 }
 view_details(){
   this.apiService.viewinventorydetails(this.userId).subscribe(res=>{
@@ -61,7 +64,13 @@ showrows(){
 addrow(){
 this.rows.push({itemId:0,itemname:"",itemqty:0});
 }
-removerow(data:any){
-  this.rows.splice(data,1);
+removerow(data:any,item:any){
+
+  if(item.itemId==0){
+    this.rows.splice(data,1);
+  }else{
+      this.deleterows.push(item);
+      
+  }
 }
 }
